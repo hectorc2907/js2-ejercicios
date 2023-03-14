a08d80390800170f0e7909e9953866d3fa0c6be9
hectorc2907 <hectorc2907@gmail.com>
2023-03-13 22:32:39 -0300

    se realiza maquetado del ejercicio 1, se comprueba funcionamiento y modelo responsive

---
+++ js/ejercicio1.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("Ingrese la edad del usuario"));
+
+if(edad>=18 && edad<=70){
+    document.write("El usuario puede Conducir");
+}else{
+    document.write("La edad ingresada es incorrecta")
+}
