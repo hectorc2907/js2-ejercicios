51e368ebc44a3ad98e6c0f76f9fbdde4ccb34c1a
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-10-30 22:38:55 -0300

    se realiza el maquetado del ejercicio 1 y se comprueba su diseño responsive

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function ejercicio1() {
+  let mensajePunto1 = document.getElementById("mensajePunto1");
+  let edad = parseInt(prompt("Ingrese la Edad del Usuario: "));
+
+  switch (true) {
+    case edad <= 18 && edad !== 1:
+      mensajePunto1.textContent = `El usuario tiene ${edad} años no puede conducir`;
+      break;
+    case edad > 18:
+      mensajePunto1.textContent = `El usuario tiene ${edad} años puede conducir`;
+      break;
+    case edad === 1:
+      mensajePunto1.textContent = `El usuario tiene ${edad} año no puede conducir`;
+      break;
+    default:
+      mensajePunto1.textContent = `La entrada no es valida, pruebe una vez mas`;
+      break;
+  }
+}
+let punto1 = document.getElementById("btnPunto1");
+punto1.addEventListener("click", ejercicio1);
